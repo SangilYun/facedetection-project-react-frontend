7f0e233dfba7be7771969d5ec679a3495ead1a22
SangilYun <nasuler@gmail.com>
2019-02-17 23:12:47 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
     .then(response =>response.json())
     .then(response => {
       if(response){
-        fetch('https://sangil-fd-api.herokuapp.com:3000/image',{
+        fetch('https://sangil-fd-api.herokuapp.com/image',{
           method : 'put',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify({
