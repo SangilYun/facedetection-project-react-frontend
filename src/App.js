d475b444635a3eb48d88294b8d9a6993c04822ea
SangilYun <nasuler@gmail.com>
2019-02-17 19:52:51 +0900

    add input validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
@@ -10,10 +9,6 @@ import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
-
-const app = new Clarifai.App({
- apiKey: 'dd67f765be9643ca8b3fbd6962508e7c'
-});
 
 
 const particlesOptions = {
@@ -85,9 +80,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl : this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+    fetch('http://localhost:3000/imageurl',{
+      method : 'post',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        input : this.state.input
+      })
+    })
+    .then(response =>response.json())
     .then(response => {
       if(response){
         fetch('http://localhost:3000/image',{
