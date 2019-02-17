75907df5cc9d09acd2690f66076ff4dc385870a5
SangilYun <nasuler@gmail.com>
2019-02-17 22:48:07 +0900

    deploy to heroku

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl : this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://sangil-fd-api.herokuapp.com/imageurl',{
       method : 'post',
       headers: {'Content-Type' : 'application/json'},
       body: JSON.stringify({
@@ -90,7 +90,7 @@ class App extends Component {
     .then(response =>response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3000/image',{
+        fetch('https://sangil-fd-api.herokuapp.com:3000/image',{
           method : 'put',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component{
 	}
 
 	onSubmitSignIn=()=>{
-		fetch('http://localhost:3000/register', {
+		fetch('https://sangil-fd-api.herokuapp.com/register', {
 			method : 'post',
 			headers: {'Content-Type' : 'application/json'},
 			body: JSON.stringify({
